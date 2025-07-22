7680f1931c619c8d767ac9dfc5881da647730e3d
nelvuspam <hdnelvu@gmail.com>
2025-07-22 21:00:24 +0200

    postcard sender

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         SmsSender smsSender = new SmsSender();
         EmailSender emailSender = new EmailSender();
+        PostcardSender postcardSender = new PostcardSender();
         Dog dog = new Dog("fafik", smsSender, emailSender);
         dog.setName("sasza");
         dog.setName("paweł");
---
+++ src/PostcardSender.java
@@ -0,0 +1,5 @@
+public class PostcardSender {
+    public void sendPostcard(Animal animal, String oldName, String newName){
+        System.out.println("[POSTCARD] Zwierze zmieniło imie z " + oldName + " na " + newName);
+    }
+}
